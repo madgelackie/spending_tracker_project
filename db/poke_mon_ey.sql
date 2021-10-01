be07fad24a69adec456b1aaf2607cce2b66f00a4
madgelackie <marionmlackie@gmail.com>
2021-10-01 12:57:53 +0100

    db tables working

---
+++ db/poke_mon_ey.sql
@@ -0,0 +1,22 @@
+DROP TABLE transactions;
+DROP TABLE tags;
+DROP TABLE merchants;
+
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY,
+    type VARCHAR(255),
+    active 
+);
+
+CREATE TABLE merchants (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    active BOOLEAN
+);
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    amount INT,
+    tag_id INT REFERENCES tags(id) ON DELETE CASCADE,
+    merchant_id INT REFERENCES merchants(id) ON DELETE CASCADE
+);
