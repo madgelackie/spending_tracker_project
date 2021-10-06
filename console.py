b7311bca532b2d47bd5af1a20f613dc56b214ba4
madgelackie <marionmlackie@gmail.com>
2021-10-06 11:14:54 +0100

    ready to go

--- console.py
+++ console.py
@@ -23,6 +23,10 @@ tag_repository.save(tag3)
 tag4 = Tag('tournament fees')
 tag_repository.save(tag4)
 
+tag5 = Tag('clothing', False)
+tag_repository.save(tag5)
+
+
 merchant1 = Merchant('Supermercado', True)
 merchant_repository.save(merchant1)
 
@@ -35,11 +39,17 @@ merchant_repository.save(merchant3)
 merchant4 = Merchant('Pokédex')
 merchant_repository.save(merchant4)
 
+merchant5 = Merchant("Aina's Kitchen", False)
+merchant_repository.save(merchant5)
+
 transaction1 = Transaction(2500, tag1, merchant1, '20200115')
 transaction_repository.save(transaction1)
 
 transaction2 = Transaction(300, tag2, merchant4, '20210205')
 transaction_repository.save(transaction2)
+
+transaction3 = Transaction(1000, tag4, merchant3, '20210629')
+transaction_repository.save(transaction3)
 
 
 pdb.set_trace()
